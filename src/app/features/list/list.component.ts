df97678e42c9f0e066c87b4f256d87d252116251
Pedro Neves <55400486+pdr-tuche@users.noreply.github.com>
2024-09-09 23:04:07 -0300

    criado cards

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {
+  products: Product[] = [];
+
+  productService = inject(ProductsService);
+  ngOnInit() {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
